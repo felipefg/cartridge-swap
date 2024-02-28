26cd139394863eeeb36485ecb115966a331c1556
Eduardo Bart <edub4rt@gmail.com>
2024-02-28 00:24:21 -0300

    Sell button

--- frontend/app/components/CartridgeDescription.tsx
+++ frontend/app/components/CartridgeDescription.tsx
@@ -8,8 +8,8 @@ import { fontPressStart2P } from '../utils/font';
 
 import { ContractReceipt, ethers } from "ethers";
 import { useConnectWallet } from "@web3-onboard/react";
-import { BuyCartridgePayload} from '../backend-libs/app/ifaces';
-import { buyCartridge } from '../backend-libs/app/lib';
+import { BuyCartridgePayload, SellCartridgePayload } from '../backend-libs/app/ifaces';
+import { buyCartridge, sellCartridge } from '../backend-libs/app/lib';
 import { envClient } from "../utils/clientEnv";
 import { balanceContext } from '../components/balanceProvider';
 
@@ -37,8 +37,8 @@ function CartridgeDescription() {
     async function sell() {
         try {
             const signer = new ethers.providers.Web3Provider(wallet.provider, 'any').getSigner();
-            // const payload: SellCartridgePayload = {id: "0x"+selectedCartridge.id};
-            // const receipt = await sellCartridge(signer, envClient.DAPP_ADDR, inputData, {decode:true, cartesiNodeUrl: envClient.CARTESI_NODE_URL});
+            const payload: SellCartridgePayload = {id: "0x"+selectedCartridge.id};
+            const receipt = await sellCartridge(signer, envClient.DAPP_ADDR, payload, {sync:true, cartesiNodeUrl: envClient.CARTESI_NODE_URL});
             await updateWalletBalance();
             await updateCartridge();
         } catch (error) {
@@ -70,7 +70,7 @@ function CartridgeDescription() {
                     <h2 className='text-lg text-gray-700 mt-4'>Sell Price</h2>
                     <pre className={fontPressStart2P.className}>
                         <span className="text-lg text-blue-700">${(selectedCartridge.sell_price / 1000000).toFixed(2)}</span>
-                        <button disabled={!wallet} className="btn w-1/3 ml-1" onClick={sell}>
+                        <button disabled={!wallet || !(selectedCartridge.owned_copies > 0)} className="btn w-1/3 ml-1" onClick={sell}>
                             SELL
                         </button>
                     </pre>
